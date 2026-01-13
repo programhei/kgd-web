b22ef40e078dd9c5960428fa7612c795ec49cbab
programhe <2048446891@qq.com>
2026-01-13 17:53:07 +0800

    添加

--- apps/backend-mock/api/auth/codes.ts
+++ apps/backend-mock/api/auth/codes.ts
@@ -1,6 +1,6 @@
 import { verifyAccessToken } from '~/utils/jwt-utils';
 import { unAuthorizedResponse } from '~/utils/response';
-
+// 模拟验证码接口
 export default eventHandler((event) => {
   const userinfo = verifyAccessToken(event);
   if (!userinfo) {
